4ca18dec22273f881982616d8697ba0920b4c662
Vishwa Shankar <vishwas1020@gmail.com>
2020-06-04 16:48:36 -0700

    navbar icon redirect to landing

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
+import {useHistory} from 'react-router-dom';
 
 import './nav.css';
 import YTLogo from '../assets/yt-dark.jpg';
 
 function NavBar(){
+    const history = useHistory();
+    const imageClick=()=>{
+        history.push('/');
+    }
 return(
+    
     <nav>
-        <img src={YTLogo}></img>
+        <img src={YTLogo} onClick={() => imageClick()}></img>
         <h1 className='nav-title'>Watch Party</h1>
     </nav>
 );
